0a37065b85e41c4df8d7e0842d0f01d0362aced6
symbiancross <atomicsecure@gmail.com>
2017-03-13 11:24:47 +0700

    your name

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<h1>ToDo List</h1>
+	<ul>
+		<li>Taki</li>
+		<li>Mitsuha</li>
+		<li>Yotsuha</li>
+	</ul>
+	<form>
+		<input type="text" name="todo_item">
+		<button type="submit">Add</button>
+	</form>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
